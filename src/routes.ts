53d9df72cb6b7d9da52e02d0c40c5789b48500ab
Francisco de Assis <contato@franciscoprog.com>
2022-12-24 13:07:20 -0300

    Cadastro de categoria

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import  {CreateUserController} from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 
 const router = Router();
@@ -12,5 +13,7 @@ router.post('/users', new CreateUserController().handle)
 router.post('/login', new AuthUserController().handle)
 
 router.get('/info',isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export {router};
