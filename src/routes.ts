14db6adb0ca2d909845f4a93f4ee992dc794fde5
Francisco de Assis <contato@franciscoprog.com>
2022-12-27 17:14:16 -0300

    7th

--- src/routes.ts
+++ src/routes.ts
@@ -1,30 +1,52 @@
 import {Router} from 'express'
 import multer from 'multer';
-import  {CreateUserController} from './controllers/user/CreateUserController'
+import uploadConfig from './config/multer'
+
+import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 import { CreateProductsController } from './controllers/product/CreateProductcontroller';
-import uploadConfig from './config/multer'
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderCntroller } from './controllers/order/FinishOrderController';
+
+
 
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"));
 
+//Rotas usuario
 router.post('/users', new CreateUserController().handle)
-
 router.post('/login', new AuthUserController().handle)
-
 router.get('/info',isAuthenticated, new DetailUserController().handle)
 
+//rotas categoria
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
+//rotas produto
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductsController().handle)
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
+//rotas pedido
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrderController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderCntroller().handle)
 
 
 export {router};
